991544c03a160b608c2243e45c29f8856640ca2c
Pawel <pawelurbanik94@gmail.com>
2021-10-18 11:26:48 +0200

    Repo changes

--- src/main/java/pl/pawel/shoppinglistapp2/repository/ProductRepository.java
+++ src/main/java/pl/pawel/shoppinglistapp2/repository/ProductRepository.java
@@ -6,5 +6,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import pl.pawel.shoppinglistapp2.model.Product;
 
 @Repository
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository  extends JpaRepository<Product, Integer> {
 }
--- src/main/java/pl/pawel/shoppinglistapp2/repository/ShoppingListRepository.java
+++ src/main/java/pl/pawel/shoppinglistapp2/repository/ShoppingListRepository.java
@@ -2,8 +2,10 @@ package pl.pawel.shoppinglistapp2.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import pl.pawel.shoppinglistapp2.model.ShoppingList;
 
 @Repository
+@CrossOrigin("http://localhost:4200")
 public interface ShoppingListRepository extends JpaRepository<ShoppingList, Long> {
 }
